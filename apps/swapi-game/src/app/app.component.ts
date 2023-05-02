70f716b8710ca4a92a515d0fc57af8bcd84cc7aa
Andrew Allen <andrewallen1.6180@yahoo.co.uk>
2023-05-02 16:16:25 +0100

    chore(swapi-game): generate app

---
+++ apps/swapi-game/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  imports: [],
+  selector: 'ng-signal-form-root',
+  template: `<h1>Welcome swapi-game</h1> `,
+  styles: [''],
+})
+export class AppComponent {}
